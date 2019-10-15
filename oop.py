59f1c1a5b1540fecdb5b9a5772d4e992caa890f6
badhbh <annspb1@mail.ru>
2019-10-15 21:28:24 +0300

    first commit

---
+++ oop.py
@@ -0,0 +1,53 @@
+class Song:
+    def __init__(self, name, subgenre):
+        self.name = name
+        self.subgenre = subgenre
+
+class Album:
+    def __init__(self,  name):
+        self.name = name
+        self.songs = []
+
+    def add_song(self, song):
+        self.songs.append(song)
+
+    def get_size_of_album(self):
+        return len(self.songs)
+
+class Artist:
+    def __init__(self, name):
+        self.name = name
+        self. creatures = []
+
+    def add_album(self, album):
+        self.creatures.append(album)
+
+    def number_of_albums(self):
+        return len(self.creatures)   
+
+class Album_collection:
+    def __init__(self):
+        self.albums = []
+
+    def add_album(self, album):
+        self.albums.append(album)
+
+    def size_of_album_coll(self):
+        return len(self.albums)
+
+
+name_s1 = "jojo"
+subg1 = "jo"
+song = Song(name_s1, subg1)
+name_a1 = "hhh"
+album = Album(name_a1)
+album.add_song(song.name)
+name_ar1 = "Reno"
+artist = Artist(name_ar1)
+album_collection = Album_collection()
+album_collection.add_album(album)
+print(song.name, album.name, album.songs[0], artist.name,
+album_collection.albums[0].name)
+
+
+
---
+++ oop_test.py
@@ -0,0 +1,15 @@
+import unittest
+import oop
+
+class TestSum(unittest.TestCase):
+    def test_oop(self):
+        #song_j  = "jjj"
+        #subg_j  = "jj"
+        #sog = oop.Song(song_j, subg_j)
+       # self.assertEqual()
+       self.alb_c = oop.Album_collection()
+
+print("ok")
+
+if __name__ == '__main__':
+    unittest.main()
